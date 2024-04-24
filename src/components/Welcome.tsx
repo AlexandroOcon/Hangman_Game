1d8ceddd456783498521b8f1cc602f55d98d3f8d
Alexandro <neongamerhd10@gmail.com>
2024-04-24 01:07:10 -0600

    firts commit

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,24 @@
+import hangman from '../img/hangman2.png';
+import '../css/Welcome.css'
+
+
+interface welcomeProps {
+    onStart: () => void; // Definimos la interfaz para onStart
+}
+
+function Welcome({ onStart }: welcomeProps) {
+
+    return (
+        <>
+            <div className="Wrapper">
+                <h1>Welcome TO HANGMAN GAME</h1>
+                <h2>The Classic Game</h2>
+                <img src={hangman} alt="hangman image..." width={200} height={200}></img>
+                <div>
+                    <button className='Boton' onClick={onStart}>INICIAR JUEGO</button>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Welcome; 
